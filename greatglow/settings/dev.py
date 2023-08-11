5452281a41aa31f1a53d81b598aaa4080b31c699
Aironne Mark Quintela <aironne.quintela25@gmail.com>
2023-08-11 14:30:13 +0800

    Initial commit

---
+++ greatglow/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-5*rm3oab=58&^t=2_ll!o8w$72@s44v@gnvxw9*rb=--6pxnlb"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
